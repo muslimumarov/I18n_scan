48a67b557320ba132d14cf8abc8fe4d1c42b9ed2
muslimumarov <norbekumarov613gmail.com>
2025-01-22 16:36:24 +0500

    ldfjkdfjd

--- src/i18n/i18.ts
+++ src/i18n/i18.ts
@@ -1,8 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 import HttpBackend from 'i18next-http-backend';
 
 i18n
+.use(LanguageDetector)
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
